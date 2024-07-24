f4f2d68c7b0156d162abf30f0b9db1df2431252d
Cleimer ANT <cleimer2020@gmail.com>
2024-07-25 01:58:02 +0400

    feat: obtener alimentos por id

--- frontproyectogym/src/Components/EditarAlimento/EditarAlimento.jsx
+++ frontproyectogym/src/Components/EditarAlimento/EditarAlimento.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import {useForm} from 'react-hook-form'
 import { AuthContext } from "../../Auth/AuthContext";
 import CampoInput from '../CampoInput/CampoInput';
@@ -6,12 +6,47 @@ import { useParams } from 'react-router-dom';
 import Contenedor from '../Contenedor/Contenedor';
 import swal from 'sweetalert';
 import EditarDataAutorizacion from '../../Datos/EditarDataAutorizacion';
+import getDatosUser from '../../Datos/ObtenerCalculoCalorias';
 
 
 export default function EditarAlimento(){
     const {user} = useContext(AuthContext)
+    const [alimento, setAlimento] = useState([]);
     const {register, reset, handleSubmit, formState : {errors}} = useForm();
     const {alimentoId} = useParams()
+
+    const obtenerAlimento = async () => {
+        const url = `https://localhost:7051/api/v1/Alimentos/Obtener Alimentos por el Id?id=${alimentoId}`;
+        try{
+            const response = await getDatosUser(url, user.jwToken)
+            setAlimento(response)
+        }
+        catch(err){
+            throw err
+        }
+    }
+
+    useEffect(() => {
+        obtenerAlimento()
+    }, [])
+    
+    useEffect(() => {
+        if (alimento) {
+            reset({
+                nombreAlimento: alimento.nombreAlimento,
+                porcion: alimento.porcion,
+                calorias: alimento.calorias,
+                grasa: alimento.grasa,
+                carbohidratos: alimento.carbohidratos,
+                proteina: alimento.proteina,
+                descripcion: alimento.descripcion
+            });
+        }
+    }, [alimento]);
+    
+
+    console.log(alimento)
+
     const onSubmit = handleSubmit(async (data) => {
         console.log(data)
         const url = `https://localhost:7051/api/v1/Alimentos/Editar Alimentos?id=${alimentoId}`;
